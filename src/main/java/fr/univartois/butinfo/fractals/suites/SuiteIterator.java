a1a88b07463973fca8083940741ee03b0dd508f7
Jonathan Isambourg <jonathan.isambourg1@gmail.com>
2022-10-24 10:49:37 +0200

    Implémentation de l'Iterator sur les suites

---
+++ src/main/java/fr/univartois/butinfo/fractals/suites/SuiteIterator.java
@@ -0,0 +1,53 @@
+package fr.univartois.butinfo.fractals.suites;
+
+import fr.univartois.butinfo.fractals.complex.Complex;
+
+public class SuiteIterator implements ComplexIterator {
+    private SuitesStrategy strategy;
+
+    private int nbIterations = 0;
+
+    private int iterationsLimit;
+
+    private List<Complex> complexes = new ArrayList<>();
+
+
+    public SuiteIterator(SuitesStrategy strategy, int iterationsLimit){
+        this.strategy = strategy;
+        this.iterationsLimit = iterationsLimit;
+    }
+
+    public boolean hasNext() {
+        if (nbIterations < iterationsLimit) {
+            if (nbIterations == 0) {
+                return (strategy.getFirstElement().abs() <= 2);
+            } else {
+                return (complexes.get(nbIterations).abs() <= 2);
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public Complex getNext(){
+        if (!hasNext()){
+            return null;
+        }
+
+        Complex next;
+        if (nbIterations == 0){
+            next = strategy.getFirstElement();
+        } else {
+            previous = complexes.get(nbIterations);
+            next = strategy.calculateNextTerm(previous);
+        }
+
+        complexes.add(next);
+        nbIterations++;
+        return next;
+    }
+
+    public void reset(){
+        nbIterations = 0;
+    }
+}

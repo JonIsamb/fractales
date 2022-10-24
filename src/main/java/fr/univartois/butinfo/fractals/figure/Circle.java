8a0cf35e4bde80f015d0687e7d775f25eadb021b
Axel Poteau <axelpoteau19@gmail.com>
2022-10-24 16:34:33 +0200

    debut repr figure

---
+++ src/main/java/fr/univartois/butinfo/fractals/figure/Circle.java
@@ -0,0 +1,20 @@
+package fr.univartois.butinfo.fractals.figure;
+
+import fr.univartois.butinfo.fractals.complex.Complex;
+
+public class Circle implements IFigure{
+    private Object color;
+    private Complex pos;
+    private int radius;
+
+    public Circle(Object color, Complex pos, int radius) {
+        this.color = color;
+        this.pos = pos;
+        this.radius = radius;
+    }
+
+    @Override
+    public String repr() {
+        return "<circle cx="+pos.getRealPart()+" cy="+pos.getImaginaryPart()+" r="+radius+"/>";
+    }
+}

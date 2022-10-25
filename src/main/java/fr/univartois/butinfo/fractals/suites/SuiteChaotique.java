e2ab1e11521814914c0eb671fb25ed3a2a52c5ce
Amaury Bonsigne <amaury_bonsigne@ens.univ-artois.fr>
2022-10-25 14:44:24 +0200

    Creation de la classe abstract SuiteChaotique

--- src/main/java/fr/univartois/butinfo/fractals/suites/SuiteChaotique.java
+++ src/main/java/fr/univartois/butinfo/fractals/suites/SuiteChaotique.java
@@ -1,2 +1,19 @@
-package fr.univartois.butinfo.fractals.suites;public class SuiteChaotique {
+package fr.univartois.butinfo.fractals.suites;
+
+import java.util.Iterator;
+
+public abstract class SuiteChaotique implements ISuitesChaotique,Iterable<IPointPlan>{
+    private IPointPlan premier;
+    private int nbMaxIterations;
+
+    public SuiteChaotique(IPointPlan premier,int nbMaxIterations){
+        this.nbMaxIterations=nbMaxIterations;
+        this.premier=premier;
+    }
+
+    public Iterator<IPointPlan> iterator(){
+        return new SuiteChaotiqueIterator(this,premier,nbMaxIterations);
+    }
+
+    public abstract double getNext(IPointPlan point);
 }

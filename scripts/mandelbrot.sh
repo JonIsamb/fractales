4cae5fce1cab1115c7490079f59247c53936aee6
Jonathan Isambourg <jonathan.isambourg1@gmail.com>
2022-10-28 13:45:15 +0200

    Modification des paramètres du script mandelbrot.sh

--- scripts/mandelbrot.sh
+++ scripts/mandelbrot.sh
@@ -17,10 +17,10 @@ focus=( $focusX $focusY )
 for par in "${focus[@]}"
 do
   if [[ $par =~ ^[+-]?[0-9]+$ ]] || [[ $n =~ ^[+-]?[0-9]*\.?[0-9]+$ ]];then
-    java -jar ../build/libs/sae-2022-2023.jar -f 'm' -h 1080 -n 30 -o '../mandelbrot/mandelbrot.png' -p "$palette" -s 0.0015 -w 1080 -x $focusX -y $focusY
+    java -jar ../build/libs/sae-2022-2023.jar -f 'm' -h 1000 -n 30 -o '../mandelbrot/mandelbrot.png' -p "$palette" -s 0.0027 -w 1000 -x $focusX -y $focusY
   else
     echo "Mauvais arguments, des paramètres par défaut ont été utilisés.";
-    java -jar ../build/libs/sae-2022-2023.jar -f 'm' -h 1080 -n 30 -o '../mandelbrot/mandelbrot.png' -p "$palette" -s 0.0015 -w 1080 -x 0 -y 0
+    java -jar ../build/libs/sae-2022-2023.jar -f 'm' -h 1000 -n 30 -o '../mandelbrot/mandelbrot.png' -p "$palette" -s 0.0027 -w 1000 -x 0 -y 0
   fi
 done
 

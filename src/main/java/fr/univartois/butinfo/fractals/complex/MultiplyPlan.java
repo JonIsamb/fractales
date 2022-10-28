6db17d9729ce742f525c6661ff462bf600c699f0
Amaury Bonsigne <amaury_bonsigne@ens.univ-artois.fr>
2022-10-28 13:24:49 +0200

    documentation de la classe MultiplyPlan

--- src/main/java/fr/univartois/butinfo/fractals/complex/MultiplyPlan.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/MultiplyPlan.java
@@ -1,16 +1,39 @@
 package fr.univartois.butinfo.fractals.complex;
 
+/**
+ * Declaration de la classe MultiplyPlan qui etend le Plan et qui implemente l'interface IPlan
+ * @author Axel Poteau
+ */
 public class MultiplyPlan extends Plan implements IPlan {
+    /**
+     * Declaration d'un double zoom
+     */
     private double zoom;
+    /**
+     * déclaration d'une instance de IPlan plan
+     */
     private IPlan plan;
 
 
+    /**
+     * Déclaration du constructeur de MultiplyPlan
+     * @param zoom Attribut zoom
+     * @param plan Attribut plan
+     * @param height Attribut height récupere via super
+     * @param width Attribut width recuperer via super
+     */
     public MultiplyPlan(double zoom, IPlan plan, int height, int width) {
         super(height,width);
         this.zoom= zoom;
         this.plan = plan;
     }
 
+    /**
+     * Implementation de la méthode asComplex qui transforme une colonne et une rangée en complexe
+     * @param row Abcisse
+     * @param column Ordonné
+     * @return Un complexe
+     */
     @Override
     public IComplex asComplex(int row, int column) {
         return plan.asComplex(row,column).multiply(zoom);

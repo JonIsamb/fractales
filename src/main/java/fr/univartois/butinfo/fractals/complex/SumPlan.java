e6af074649f6cf52e94d6e3305deb2539d704e68
Amaury Bonsigne <amaury_bonsigne@ens.univ-artois.fr>
2022-10-28 12:38:21 +0200

    documentation de la classe SumPlan

--- src/main/java/fr/univartois/butinfo/fractals/complex/SumPlan.java
+++ src/main/java/fr/univartois/butinfo/fractals/complex/SumPlan.java
@@ -1,11 +1,27 @@
 package fr.univartois.butinfo.fractals.complex;
 
+/**
+ * Création de la classe SumPlan qui extends Plan et implements Iplan, elle sert a faire des translations dans un plan
+ * @author Axel Poteau
+ */
 public class SumPlan extends Plan implements IPlan{
+    /**
+     * Declaration de l'attribut cons instance de IComplex
+     */
     private IComplex cons;
+    /**
+     * Declaration de l'attribut plan instance de IPlan
+     */
     private IPlan plan;
 
 
-
+    /**
+     * Déclaration du constructeur de SumPlan de la classe SumPlan
+     * @param height Attribut Height via le super
+     * @param width attribut width via le super
+     * @param cons attribut cons
+     * @param plan attribut plan
+     */
     public SumPlan(int height, int width, IComplex cons, IPlan plan) {
 
         super(height,width);
@@ -14,6 +30,12 @@ public class SumPlan extends Plan implements IPlan{
 
     }
 
+    /**
+     * Methode asComplex qui transforme un pixel en complex
+     * @param row Abcisse
+     * @param column ordonée
+     * @return Un complexe
+     */
     @Override
     public IComplex asComplex(int row, int column) {
         return plan.asComplex(row,column).add(cons);
